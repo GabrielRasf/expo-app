1d2ad53e1fe4190f3ba8503258527914bd427400
GabrielR <gabrielrasf@hotmail.com>
2025-09-25 15:16:42 -0300

    First commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const HomePage = () => {
+  return  (
+
+    <View> 
+    
+      <Text> Primeiro texto </Text>
+    
+    </View>
+    
+  )
+}
+
+export default HomePage
